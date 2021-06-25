c2a5f0b876d48ab313476ef4551820796a710358
Amna Zeeshan <amnazee698@gmail.com>
2021-06-26 02:16:12 +0500

    Done my part

--- app/src/main/java/com/example/fitdontquit/WorkoutsActivity.java
+++ app/src/main/java/com/example/fitdontquit/WorkoutsActivity.java
@@ -5,13 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class WorkoutsActivity extends AppCompatActivity {
+    ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workouts);
         getSupportActionBar().hide();
+        img=findViewById(R.id.imgBack);
+
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),HomeScreen.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     public void buttonMountain(View view)
